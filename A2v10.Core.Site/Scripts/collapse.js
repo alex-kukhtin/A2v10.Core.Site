a23381f49fce294e59e8d64da289cd0146efb2cb
Аlex Kukhtin <alex.kukhtin@outlook.com>
2020-05-20 22:20:54 +0300

    2020 may 05 22:20

--- A2v10.Core.Site/Scripts/collapse.js
+++ A2v10.Core.Site/Scripts/collapse.js
@@ -1,16 +1,4 @@
-(function () {
-	if (window.IntersectionObserver && window.IntersectionObserverEntry) {
-		new IntersectionObserver(entries => {
-			let dy = entries[0].boundingClientRect.y;
-			if (dy < 0)
-				document.body.classList.add('has-scroll');
-			else
-				document.body.classList.remove('has-scroll');
-		})
-		.observe(document.querySelector("#scrollspy"));
-	}
-})();
-(function () {
+﻿(function () {
 
 	const SELECTOR_DATA_TOGGLE = '[data-toggle="collapse"]'
 
---
+++ A2v10.Core.Site/Scripts/scrollspy.js
@@ -0,0 +1,13 @@
+﻿
+(function () {
+	if (window.IntersectionObserver && window.IntersectionObserverEntry) {
+		new IntersectionObserver(entries => {
+			let dy = entries[0].boundingClientRect.y;
+			if (dy < 0)
+				document.body.classList.add('has-scroll');
+			else
+				document.body.classList.remove('has-scroll');
+		})
+		.observe(document.querySelector("#scrollspy"));
+	}
+})();
